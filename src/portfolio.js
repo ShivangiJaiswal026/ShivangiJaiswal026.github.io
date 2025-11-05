3bb3911ba3be0a77f8091fb833463cf039c75f01
ShivangiJaiswal026 <your_github_email@example.com>
2025-11-05 21:31:12 +0530

    Update portfolio details

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,8 +20,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "SHIVANGI J",
-  title: "Hi all, I'm SHIVANG Jasiwal",
+  username: "SHIVAssNGI J",
+  title: "Hi all, I'm SHIVANGssss Jasiwal",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
